d884c63747e528e10dc2863daa5a33894ec8d263
David Cohen <73124192+davidcoh464@users.noreply.github.com>
2022-02-08 12:57:54 +0200

    Delete Project_part1_5173_9142 directory

--- Project_part1_5173_9142/app/src/main/java/com/example/project_part1_5173_9142/DeliveryInfo.kt
+++
@@ -1,6 +0,0 @@
-package com.example.project_part1_5173_9142
-
-data class DeliveryInfo(val name:String? = null,val address:String? = null,val size:String? = null
-                        ,val weight:String? = null,val fragile:String? = null,val lng:String? = null,val lat:String? = null) {
-
-}
